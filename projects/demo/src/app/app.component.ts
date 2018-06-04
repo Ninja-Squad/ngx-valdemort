84fbdd67033f6323f71ec12ab752b7e71d70d906
cexbrayat <cedric@ninja-squad.com>
2018-06-04 07:00:58 +0200

    chore: add demo app, fix lint issues

---
+++ projects/demo/src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Config } from 'ngx-valdemort';
+
+function validCredentials(group: FormGroup) {
+  return group.get('password').value === group.get('confirmation').value ? null : { validCredentials: true };
+}
+
+interface Person {
+  name: string;
+  address: {
+    street: string;
+    city: string;
+  };
+}
+
+@Component({
+  selector: 'demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  form: FormGroup;
+  person: Person = {
+    name: '',
+    address: {
+      street: '',
+      city: ''
+    }
+  };
+  search: string;
+  searchCtrl = new FormControl('', Validators.pattern(/^\d*$/));
+
+  dynamicForm: FormGroup;
+
+  constructor(fb: FormBuilder, config: Config) {
+    config.errorsClasses = 'invalid-feedback';
+    this.form = fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', [Validators.required, Validators.minLength(2), Validators.pattern(/^[a-zA-Z]*$/)]],
+      age: [null, [Validators.required, Validators.min(18)]],
+      credentials: fb.group({
+        password: ['', Validators.required],
+        confirmation: ['', Validators.required]
+      }, { validator: validCredentials })
+    });
+
+    this.dynamicForm = fb.group({
+      occupations: fb.array([
+        fb.control('', Validators.required)
+      ])
+    });
+  }
+
+  get occupations(): FormArray {
+    return this.dynamicForm.get('occupations') as FormArray;
+  }
+
+  submit() {
+    if (this.form.valid) {
+      console.log('submitting ', this.form.value);
+    } else {
+      console.log('invalid: not submitting');
+    }
+  }
+
+  submitPerson() {
+  }
+}
