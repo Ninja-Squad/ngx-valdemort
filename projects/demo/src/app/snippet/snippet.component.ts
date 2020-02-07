4d258c6c32a81da64c86e08af841a9c1a42ab247
cexbrayat <cedric@ninja-squad.com>
2020-02-07 10:01:46 +0100

    chore: update to ng and cli v9.0.0
    BREAKING CHANGE: `ngx-valdemort` is now based on Angular 9.x. If you want to use it with an older version of Angular, then stick to the previous version of `ngx-valdemort`.

--- projects/demo/src/app/snippet/snippet.component.ts
+++ projects/demo/src/app/snippet/snippet.component.ts
@@ -8,7 +8,7 @@ import { PrismService } from '../prism.service';
 })
 export class SnippetComponent implements AfterViewInit {
 
-  @ViewChild('code', { static: false }) codeEl: ElementRef<HTMLElement>;
+  @ViewChild('code') codeEl: ElementRef<HTMLElement>;
 
   @Input() code = '';
   @Input() lang = '';
