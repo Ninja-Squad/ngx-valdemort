969ba22c886ecd0f0dedb38fdaf06ab951de81d4
jnizet <jb@ninja-squad.com>
2022-11-18 10:09:44 +0100

    chore: upgrade to material 15

--- projects/ngx-valdemort/src/test.ts
+++ projects/ngx-valdemort/src/test.ts
@@ -6,4 +6,7 @@ import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+  errorOnUnknownElements: true,
+  errorOnUnknownProperties: true
+});
