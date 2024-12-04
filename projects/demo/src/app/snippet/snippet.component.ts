9cae8e1790d4c1e3cb2289b502b1bd446c6b5019
cexbrayat <cedric@ninja-squad.com>
2024-12-04 09:41:36 +0100

    chore(deps): update dependency angular-eslint to v19

--- projects/demo/src/app/snippet/snippet.component.ts
+++ projects/demo/src/app/snippet/snippet.component.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component, ElementRef, input, viewChild, ViewEncapsulation, inject } from '@angular/core';
+import { Component, ElementRef, input, viewChild, ViewEncapsulation, inject } from '@angular/core';
 import { HighlighterService, SupportedLanguages } from '../highlighter.service';
 import { SnippetService } from './snippet.service';
 import { switchMap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'demo-snippet',
@@ -9,19 +10,18 @@ import { switchMap } from 'rxjs';
   styleUrls: ['./snippet.component.scss'],
   encapsulation: ViewEncapsulation.ShadowDom
 })
-export class SnippetComponent implements AfterViewInit {
-  private highlighterService = inject(HighlighterService);
-  private snippetService = inject(SnippetService);
-
+export class SnippetComponent {
   divEl = viewChild.required<ElementRef<HTMLDivElement>>('code');
 
   code = input.required<string>();
   lang = input.required<SupportedLanguages>();
 
-  ngAfterViewInit(): void {
-    this.snippetService
+  constructor() {
+    const snippetService = inject(SnippetService);
+    const highlighterService = inject(HighlighterService);
+    snippetService
       .load(this.code())
-      .pipe(switchMap(snippet => this.highlighterService.highlight(snippet, this.lang())))
+      .pipe(switchMap(snippet => highlighterService.highlight(snippet, this.lang())), takeUntilDestroyed())
       .subscribe(highlightedCode => (this.divEl().nativeElement.innerHTML = highlightedCode));
   }
 }
