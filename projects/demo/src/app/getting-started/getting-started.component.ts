ca5b0b68f4e3ec7ce8fd8e83315db46abbf05dae
jnizet <jb@ninja-squad.com>
2024-12-06 21:40:31 +0100

    demo: update the getting started part to remove the NgModule-related stuff

--- projects/demo/src/app/getting-started/getting-started.component.ts
+++ projects/demo/src/app/getting-started/getting-started.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { SnippetComponent } from '../snippet/snippet.component';
 
 @Component({
   selector: 'demo-getting-started',
   templateUrl: './getting-started.component.html',
-  styleUrls: ['./getting-started.component.scss'],
-  imports: [SnippetComponent]
+  styleUrls: ['./getting-started.component.scss']
 })
-export class GettingStartedComponent {
-  snippet = 'getting-started.snippet.ts-like';
-}
+export class GettingStartedComponent {}
